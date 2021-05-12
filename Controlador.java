51c25474c0301b1cef72b7e910e2f78627794155
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2021-05-12 13:36:26 -0600

    Jaca

--- Controlador.java
+++ Controlador.java
@@ -1,5 +1,9 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 Universidad del Valle de Guatemala
@@ -11,9 +15,18 @@ Jose Pablo Mozon
 */
 
 public class Controlador{
-    public static void main(String[] args) {
-        File file = new File("");
-        Scanner arcPacientes = new Scanner(file);
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        //Escanear el documento de las palabras traducidas
+        File file = new File("Spanish.txt");
+        Scanner doc = new Scanner(file);
+        Scanner scan = new Scanner(System.in);
+
+        //Escanear lo que se quiere traducir
+        File file2 = new File("Spanish.txt");
+        Scanner doc2 = new Scanner(file2);
+        Scanner scan2 = new Scanner(System.in);
+
+        Map<String,String> hm = new HashMap<String, String>();
+
     }
 }
