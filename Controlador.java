071b37d7a3acaae5f714c49df825b098204dafe7
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2021-05-11 23:00:18 -0600

    Create Controlador.java

---
+++ Controlador.java
@@ -0,0 +1,13 @@
+/*
+Universidad del Valle de Guatemala
+Algoritmos y estructuras de datos
+Hoja de trabajo 9
+Driver
+Jorge Caballeros Pérez
+Jose Pablo Mozon
+*/
+public class Controlador{
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
